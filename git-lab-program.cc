3089613482273ae267f96e8c7a61f87d1b50f65e
Ln077218 <ln077218@ohio.edu>
2020-01-22 19:39:34 -0500

    git-program-lab.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,22 @@
+/**
+*       File: git-lab-program.cc
+*       Author: Lucas Nagle
+*       1/22/2020
+*Description: make a program to upload to git and have the
+* program use a for loop and pring hello git
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char const *argv[]){
+    /* add code */
+    for (int i = 0; i <= 10; ++i)
+    {
+        cout << i << endl;
+        
+    }
+    cout << "Hello git"<<endl;
+}
